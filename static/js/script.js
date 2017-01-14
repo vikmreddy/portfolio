b8dee0010bc45b61a227801f071cac146056d19a
ghvik <vreddy@ischool.berkeley.edu>
2017-01-14 14:13:17 -0800

    Made project tiles change when user hovers over them

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,28 @@ $(document).ready(function() {
 	var options = [{scrollOffset: 300}]
 	$('.scrollspy').scrollspy(options);
 });
+
+// Provides hover over project tile functionality
+// By calling the card's activator node in the DOM
+$(function() {
+    $('.card').mouseover(
+        function() {
+            $(this).find('> .card-image > img.activator').click();
+        }, 
+        function() {
+            $(this).find('> .card-reveal > .card-title').click();
+        }
+    );
+});
+
+$('.card')
+	.mouseover(
+		function(e) {
+			$(this).find('> .card-image > img.activator').click();
+		})
+	.mouseout(
+		function(e) {
+			$(this).find('> .card-reveal > .card-title').click();
+		}
+	);
+
