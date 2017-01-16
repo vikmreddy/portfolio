22ddca1e25c09ef9c49ed86fc2c5b3715186cb6c
ghvik <vreddy@ischool.berkeley.edu>
2017-01-15 16:58:51 -0800

    Tested with scrollOffset options in jquery

--- static/js/script.js
+++ static/js/script.js
@@ -7,6 +7,10 @@ $(document).ready(function() {
 // 	$('.scrollspy').scrollSpy(options);
 // });
 
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+});
+
 // Provides hover over project tile functionality
 // By calling the card's activator node in the DOM
 $('.card')
